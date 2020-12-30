20d0feb886d23985a174c8d976657ab47744a8a8
Seema Vora <seema.s.vora@gmail.com>
2020-12-29 17:16:45 -0800

    New Problem

--- Winter12_27/securedoors.py
+++ Winter12_27/securedoors.py
@@ -24,26 +24,3 @@ for x in range(logLength):
 for i in range (len(logOutput)):
   print(logOutput[i])
 
-# for x in range (logLength):
-#   entExit = str(input())
-#   logInput.append(entExit)
-#   temp = entExit.split(" ")
-#   name = temp[1]
-#   status = temp[0]
-#   if name in inBuilding and status == 'entry':
-#     print("in")
-#     logOutput.append(temp[1] +' entered (ANOMALY)')
-#     inBuilding.add(str(temp[1]))
-#   elif (name in inBuilding and status =='exit'):
-#     print("in1")
-#     logOutput.append(temp[1] +' exited')
-#     inBuilding.remove(str(temp[1]))
-#   elif (name not in inBuilding) and (status =='exit'):
-#     print("in2")
-#     logOutput.append(temp[1] +' exited (ANOMALY)')
-#   elif (name in inBuilding and status =='entered'):
-#     print("in3")
-#     logOutput.append(temp[1] +' exited')
-#     inBuilding.add(str(temp[1]))
-# print(inBuilding)
-# print(logOutput)
---
+++ Winter12_27/sevenWonders.py
@@ -0,0 +1,21 @@
+cards= str(input())
+individualCard = list(cards)
+tCount = 0
+cCount = 0
+gCount = 0
+tcgCount = 0
+for i in range (len(individualCard)):
+  if individualCard[i]== 'T':
+    tCount +=1
+  elif individualCard[i] == 'C':
+    cCount += 1
+  elif individualCard[i] == 'G':
+    gCount += 1
+minCount = min(tCount, cCount, gCount)
+if minCount ==0:
+  extraPoint = 0
+else: 
+  extraPoint = minCount *7
+
+finalSum = (tCount **2) + (cCount **2) + (gCount **2) + extraPoint
+print(finalSum)
