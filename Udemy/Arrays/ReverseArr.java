f79985e71fe7924726159a5ebc82a47bad3172b0
Seema Vora <seema.s.vora@gmail.com>
2021-01-14 15:07:59 -0800

    Adds Udem Arr Q #2

---
+++ Udemy/Arrays/ReverseArr.java
@@ -0,0 +1,20 @@
+
+public class ReverseArr {
+    public static void main(String[] args){
+    char[] inputArr = {'c','o','m','p','u','t','e','r','-','s','c','i','e','n','c','e'};
+    int len = inputArr.length;
+    int pivot = (len/2);
+    for(int i = 0; i <pivot; i++){
+      char tempL = inputArr[(len)-i-1];
+      char tempU = inputArr[i];
+
+        inputArr[i] = tempL;
+        inputArr[(len)-1-i] = tempU;
+ }
+   
+  for (int k = 0; k< len; k++){
+    System.out.print(inputArr[k]);
+  }
+  System.out.println(' ');
+    }
+}
