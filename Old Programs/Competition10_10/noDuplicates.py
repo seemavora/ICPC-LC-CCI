3983d20d78715170d77279e421266222e29c29c7
Seema Vora <seema.s.vora@gmail.com>
2021-01-14 16:18:02 -0800

    Adds Leetcode TwoSum Question and Deletes a Few Files

--- Old Programs/Competition10_10/noDuplicates.py
+++
@@ -1,7 +0,0 @@
-givenSent = str(input())
-sentArr = givenSent.split(' ')
-sentSet = set(sentArr)
-if len(sentArr) == len(sentSet):
-  print("yes")
-else:
-  print("no")
--- Old Programs/modulo.py
+++
@@ -1,8 +0,0 @@
-nums = [int(input()) for i in range (10)]
-modulo = []
-for i in range (10):
-  modulo.append(nums[i] % 42)
-
-noDups = set(modulo)
-
-print(len(noDups))
